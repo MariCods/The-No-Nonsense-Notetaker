d36b0a7a29e09c24b83249badc4e27460a694ae4
Mary Cods <maryam.selassie@gmail.com>
2023-03-14 21:45:04 -0700

    First Attempt

---
+++ Develop/db/notes.js
@@ -0,0 +1,14 @@
+const uuid = require('../helpers/uuid');
+ 
+ const notes = [
+    
+    {
+        title:'Test Title',
+        text:'Test text',
+        note_id: uuid(),
+    },
+];
+
+
+
+module.exports = notes;
