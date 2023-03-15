2fd9bafba26cefc9243892a1d0c5dbcd3bab9ff5
Mary Cods <maryam.selassie@gmail.com>
2023-03-14 22:56:02 -0700

    Node packages dwnlded but port error

--- server.js
+++ server.js
@@ -1,50 +1,56 @@
 const express = require('express');
 const path = require('path');
 
- const uuid = require('./helpers/uuid');
-const notes = require('./db/notes');
+//  const uuid = require('./helpers/uuid');
+// const notes = require('./db/notes');
 
-
-const PORT = 3001;
 const app = express();
+const PORT = 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => 
+app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
+
+app.get('/index', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/db/notes', (req, res) => {
-    console.info('Get /db/notes');
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    console.info('Get /api/notes');
     res.status(200).json(notes);
 });
 
-app.get('./db/notes/:notes_id', (req, res) => {
-    if (req.params.notes_id) {
-      console.info(`${req.method} request received to get a single a notes`);
-      const notesId = req.params.notes_id;
-      for (let i = 0; i < notes.length; i++) {
-        const currentNotes = notes[i];
-        if (currentNotes.notes_id === notesId) {
-          res.json(currentNotes);
-          return;
-        }
-      }
-      res.status(404).send('Notes not found');
-    } else {
-      res.status(400).send('Notes ID not provided');
-    }
-  });
+// app.get('/api/notes/:notes_id', (req, res) => {
+//     if (req.params.notes_id) {
+//       console.info(`${req.method} request received to get a single a notes`);
+//       const notesId = req.params.notes_id;
+//       for (let i = 0; i < notes.length; i++) {
+//         const currentNotes = notes[i];
+//         if (currentNotes.notes_id === notesId) {
+//           res.json(currentNotes);
+//           return;
+//         }
+//       }
+//       res.status(404).send('Notes not found');
+//     } else {
+//       res.status(400).send('Notes ID not provided');
+//     }
+//   });
 
-  app.post('db/notes', (req, res) => {
+  app.post('/api/notes', (req, res) => {
     
     console.info(`${req.method} request received to add a note`);
   
    
-    const { title, text,  } = req.body;
+    const { title, text } = req.body;
   
     // If all the required properties are present
     if (title && text ) {
@@ -52,7 +58,6 @@ app.get('./db/notes/:notes_id', (req, res) => {
       const newNote = {
         title,
         text,
-        notes_id: uuid(),
       };
   
       const response = {
